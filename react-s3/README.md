a1c3007b3979df94d735a86a6052e0734ccf388e
masterhung0112 <masterhung0112@gmail.com>
2022-04-12 22:42:00 +0700

    Updating react-cors-spa-stack.yaml

--- react-s3/README.md
+++ react-s3/README.md
@@ -13,6 +13,18 @@ Several services will be configured. Example, Route 53...
 - AWS CloudTrail
 - Aws CloudFormation
 
+# Deployment
+- [Deploy the AWS CloudFormation Template](#deploy-stack)
+- Get the value of `APIEndpoint` and `Bucket`
+- Copy the value of `APIEndpoint` to `src/App.js`
+- Build the application package: `yarn build`
+- Upload the content of the `build` folder into the S3 bucket identified by `Bucket`
+    - Command: `aws s3 cp --recursive build s3://react-cors-spa-4gxqbwo4b0`
+
+# Clean up
+- Delete the S3 bucket contents
+- Delete the AWS CloudFormation stack
+
 # Commands
 
 ## Validate the CloudFormation syntax
@@ -36,5 +48,3 @@ When the stack is in `ROLLBACK_COMPLETE` state, call the following command
 ```sh
 aws cloudformation delete-stack --stack-name react-cors-spa-stack
 ```
-
-# Deploy to S3
--- react-s3/src/App.js
+++ react-s3/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useFetch } from "react-async";
+
+// const APIEndPoint = 'to be replaced with your api endpoint here'
+const APIEndPoint = 'https://4gxqbwo4b0.execute-api.us-west-2.amazonaws.com/v1/hello'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is the example app loaded from S3
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <APIResult />
+        <img src={logo} className="App-MainLogo" alt="logo" />
       </header>
+      <p>
+        This react-based application is hosted in an S3 bucket exposed through a
+        CloudFront distribution
+      </p>
     </div>
   );
 }
+
+const APIResult = () => {
+  const { data, error } = useFetch(APIEndPoint, {
+    headers: { accept: "application/json" },
+  });
+  if (error) return <p>{error.message}</p>;
+  if (data) return <p>{data.message}</p>;
+  return null;
+};
 
 export default App;
